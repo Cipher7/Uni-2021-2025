9c5e11328a4c667aab8ac71f3b161452ee5f78d6
Rohan <cipher@rohannk.com>
2023-01-06 15:13:31 +0530

    Prog7 - 90% done

---
+++ 2nd Year/DS-Lab/prog7.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <math.h>
+
+void main()
+{
+	int size;
+	printf("Enter size of array : ");
+	scanf("%d", &size);
+	int a[size];
+	
+	printf("Enter %d elements : ", size);
+	for(int i=0; i<size; i++)
+	{
+		scanf("%d", &a[i]);
+	}
+	
+	int levels=0;
+	//Finding the number of levels
+	for(int i=0;;i++)
+	{
+		if(pow(2,i) >= size)
+		{
+			levels = i;
+			break;
+		}
+	}
+	
+	int ptr=0;
+
+	// Displaying using level oreder traversal for binary tree
+	for(int i=levels; i >= 0; i--)
+	{
+		//spacing
+		for(int j=0; j <= i; j++)
+		{
+			printf(" ");
+		}
+		
+		//printing values
+		for(int k=0; k < pow(2,(levels-i)) ; k++)
+		{
+			printf("%d", a[ptr]);
+			ptr++;
+			if(ptr == size)
+			{
+				printf("\n");
+				return;
+			}			
+		}
+		printf("\n");
+	}	
+}
