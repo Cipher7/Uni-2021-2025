e7b0db7a28b899660a99eab3b1c7e725e3f6906e
Rohan N K <70212373+Cipher7@users.noreply.github.com>
2023-04-05 17:43:44 +0530

    Applet Program
    Contributed by Vis

---
+++ 2nd Year/OOPS_LAB/src/Program12/DemoApplet.java
@@ -0,0 +1,11 @@
+import java.applet.Applet;
+import java.awt.Graphics;
+
+public class DemoApplet extends Applet{
+	
+	public void paint(Graphics g)
+	{
+		g.drawString("Welcome", 500, 500);
+	}
+
+}
