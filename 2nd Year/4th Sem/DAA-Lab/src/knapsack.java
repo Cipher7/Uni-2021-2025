5a6acfce0d3c09b6d3069807b7267f559f598b39
Rohan N K <70212373+Cipher7@users.noreply.github.com>
2023-07-10 14:49:26 +0530

    Update knapsack.java

--- 2nd Year/4th Sem/DAA-Lab/src/knapsack.java
+++ 2nd Year/4th Sem/DAA-Lab/src/knapsack.java
@@ -23,7 +23,106 @@ public class knapsack {
 		{
 			p_w[i] = (double)profit[i]/(double)weight[i];
 		}
+		System.out.println("");
+		System.out.println("-------------------");
+		System.out.println("-----Data-Set------");
+		System.out.print("-------------------");
+		System.out.println("");
+		System.out.print("Objects");
+		for(int i=1;i<=object;i++)
+		{
+			System.out.print(i+"    ");
+		}
+		System.out.println();
+		System.out.print("Profit ");
+		for(int i=0;i<object;i++)
+		{
+			System.out.print(profit[i]+"    ");
+		}
+		System.out.println();
+		System.out.print("Weight ");
+		for(int i=0;i<object;i++)
+		{
+			System.out.print(weight[i]+"    ");
+		}
+		System.out.println();
+		System.out.print("P/W    ");
+		for(int i=0;i<object;i++)
+		{
+			System.out.print(p_w[i]+"  ");
+		}
+		for(int i=0;i<object-1;i++)
+		{
+			for(int j=i+1;j<object;j++)
+			{
+				if(p_w[i]<p_w[j])
+				{
+					double temp=p_w[j];
+					p_w[j]=p_w[i];
+					p_w[i]=temp;
+					
+					int temp1=profit[j];
+					profit[j]=profit[i];
+					profit[i]=temp1;
+					
+					int temp2=weight[j];
+					weight[j]=weight[i];
+					weight[i]=temp2;
+			}
+			}
+		}
+System.out.println("");
+		System.out.println("-------------------");
+		System.out.println("--After Arranging--");
+		System.out.print("-------------------");
+		System.out.println("");
+		System.out.print("Objects");
+		for(int i=1;i<=object;i++)
+		{
+			System.out.print(i+"    ");
+		}
+		System.out.println();
+		System.out.print("Profit ");
+		for(int i=0;i<object;i++)
+		{
+			System.out.print(profit[i]+"    ");
+		}
+		System.out.println();
+		System.out.print("Weight ");
+		for(int i=0;i<object;i++)
+		{
+			System.out.print(weight[i]+"    ");
+		}
+		System.out.println();
+		System.out.print("P/W    ");
+		for(int i=0;i<object;i++)
+		{
+			System.out.print(p_w[i]+"  ");
+		}
+		int k=0;
+		double sum=0;
+		System.out.println();
+		while(m>0)
+		{
+			if(weight[k]<m)
+			{
+				
+				sum+=1*profit[k];
+				m=m-weight[k];
+				k++;
+			}
+else
+			{
+				
+				double x4=m*profit[k];
+				double x5=weight[k];
+				double x6=x4/x5;
+				sum=sum+x6;
+				m=0;
+			}
 		
+		}
+		System.out.println("Final Profit is="+sum);
 	}
 
 }
