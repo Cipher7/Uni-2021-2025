713db571b41bdd43e7be25117942cfb270794e12
Cipher <cipher@rohannk.com>
2023-07-15 16:22:36 +0530

    Python prog 4 a

---
+++ 2nd Year/4th Sem/Python-Lab/p4-a-merge.py
@@ -0,0 +1,37 @@
+def Merge(left, right):
+    merged = []
+    left_index, right_index = 0, 0
+
+    while left_index < len(left) and right_index < len(right):
+        if left[left_index] < right[right_index]:
+            merged.append(left[left_index])
+            left_index += 1
+        else:
+            merged.append(right[right_index])
+            right_index += 1
+
+    merged += left[left_index:]
+    merged += right[right_index:]
+
+    return merged
+
+def merge_sort(arr):
+    if len(arr) <= 1:
+        return arr
+
+    mid = len(arr) // 2
+    left_half = arr[:mid]
+    right_half = arr[mid:]
+
+    left_half = merge_sort(left_half)
+    right_half = merge_sort(right_half)
+
+    return Merge(left_half, right_half)
+
+
+l = input("Enter a list of numbers separated by spaces: ")
+unsorted = [int(num) for num in l.split()]
+
+print("Unsorted list:", unsorted)
+sorted = merge_sort(unsorted)
+print("Sorted list:", sorted)
