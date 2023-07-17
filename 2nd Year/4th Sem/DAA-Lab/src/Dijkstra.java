3dce1c15adc701ccea6334296874adcf8b0e238a
Rohan <cipher@rohannk.com>
2023-07-17 14:42:12 +0530

    Dijshtra

--- 2nd Year/4th Sem/DAA-Lab/src/Dijkstra.java
+++ 2nd Year/4th Sem/DAA-Lab/src/Dijkstra.java
@@ -1,59 +1,74 @@
 import java.util.*;
 
-public class DijkstraDemo
+public class Dijkstra 
+{
+	public static int min(int a, int b)
+	{
+		if(a<b) 
+			return a;
+		else
+			return b;
+	}
 
-public static int min(int a, int b) // method to return the minimum of two numbers
+	public static boolean check (int[] v, int n)
+	{
+		for(int i=1;i<n+1;i++)
+		{
+			if(v[i]!=1)
+				return true;
+		}
 
-{if(a<b) return a
+		return false;
+	}
 
-else
+	public static void main(String[] args) 
+	{ 
+		Scanner s = new Scanner(System.in);
+		System.out.println("Enter the number of nodes in graph"); 
+		int n = s.nextInt();
+		int i,j;
 
-return b;
+		int graph[][] = new int[n+1][n+1]; 
+		int visited[]= new int[n+1];
+		int d[]= new int[n+1];
+		TreeMap<Integer, Integer> map= new TreeMap<Integer,Integer>(); 
+		System.out.println("Enter cost adjacency matrix for graph. If two nodes are not connected enter 999");
 
-1 public static boolean check int[] vint n)
+		for (i=1;i<n+1;i++)
+		{
+			for(j=1;j<n;j++)
+			{
+				graph[i][j] = s.nextInt();
+			}
+		}
+		for(i=1;i < n+1;i++)
+		{
+			visited[i]=0; 
+			d[i]=999;
+		}
+		System.out.println("Enter the source vertex"); 
+		int source = s.nextInt(); 
+		visited[source]=1; 
+		d[source]=0;
+		int u=source; 
+		int v,a=0;
+		do
+		{
+			for (v=1;v<n+1;v++)
+			{
+				if(graph[u][v]!=999 && visited[v]!=1 && graph[u][v]!=0) 
+				{
+					d[v] = min(d[v],(d[u]+graph[u][v])); 
+					map.put(d[v],v);
+				}
+			}
+			u=map.firstEntry().getValue(); 
+			visited[u]=1;
+			map.clear();
+		}
 
-{for(int i=1;i<n+1;i++)
-
-{v[i]=1) return true;
-
-1
-
-return false;
-
-public static void main(String[] args) { Scanner s-new Scanner(System.in);
-
-System.out.println("Enter the number of nodes in graph"); int ns.nextInt();
-
-intij
-
-int graph-new int[n+1][n+1); int visited-new int[n+1):
-
-int d[]= new int[n+11:
-
-TreeMap<Integer, Integer> map= new TreeMap<Integer Integer>(); System.out.println("Enter cost
-
-adjacency matrix for graph. If two nodes are not connected enter 999");
-
-for(i=1;i<n+1;i++)
-
-(visited[i]-0; d[i]-999;
-
-System.out.println("Enter the source vertex"); int source-s nextint(): visited[source]=1; d[source] 0:
-
-int u-source; int v,a=0;
-
-do // Calculating the d[] array
-
-if(graph|u|[v]-999 && visited[v]-1 && graph[u][v]!=0) {d[v]-min{d[v]{d[u]+graph[u][v]]); map.put(d[v].v)
-
-1
-
-J
-
-u-map.firstEntry().getValue(); visited[u]=1;
-
-map.clear();
-
-while(check(visited,n)); for(i=1;i<n+1;i++)
-
-System.out.println("Distance from source to ++is+d
+		while(check(visited,n)); 
+		for(i=1;i<n+1;i++)
+			System.out.println("Distance from source to" + i + " is " +d[i]);
+	}
+}
