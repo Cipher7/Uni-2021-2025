d947099325cedae432185c95f1f226db28183ff6
Rohan N K <70212373+Cipher7@users.noreply.github.com>
2023-07-17 14:48:21 +0530

    Update Dijkstra.java

--- 2nd Year/4th Sem/DAA-Lab/src/Dijkstra.java
+++ 2nd Year/4th Sem/DAA-Lab/src/Dijkstra.java
@@ -1,1 +1,65 @@
 import java.util.*;
+
+public class Dijkstra {
+    public static int min(int a, int b) {
+        if (a < b)
+            return a;
+        else
+            return b;
+    }
+
+    public static boolean check(int[] v, int n) {
+        for (int i = 1; i < n + 1; i++) {
+            if (v[i] == 0)
+                return true;
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter the number of nodes in the graph");
+        int n = s.nextInt();
+        int i, j;
+
+        int graph[][] = new int[n + 1][n + 1];
+        int visited[] = new int[n + 1];
+        int d[] = new int[n + 1];
+        TreeMap<Integer, Integer> map = new TreeMap<Integer, Integer>();
+        System.out.println("Enter cost adjacency matrix for the graph. If two nodes are not connected, enter 999");
+
+        for (i = 1; i < n + 1; i++) {
+            for (j = 1; j < n + 1; j++) {
+                graph[i][j] = s.nextInt();
+            }
+        }
+        for (i = 1; i < n + 1; i++) {
+            visited[i] = 0;
+            d[i] = 999;
+        }
+        System.out.println("Enter the source vertex");
+        int source = s.nextInt();
+        visited[source] = 1;
+        d[source] = 0;
+        int u = source;
+        int v, a = 0;
+        do {
+            for (v = 1; v < n + 1; v++) {
+                if (graph[u][v] != 999 && visited[v] != 1 && graph[u][v] != 0) {
+                    d[v] = min(d[v], (d[u] + graph[u][v]));
+                    map.put(d[v], v);
+                }
+            }
+            if (!map.isEmpty()) {
+                u = map.firstEntry().getValue();
+                visited[u] = 1;
+                map.pollFirstEntry();
+            }
+        } while (check(visited, n));
+
+        for (i = 1; i < n + 1; i++)
+            System.out.println("Distance from source to " + i + " is " + d[i]);
+    }
+}
+
