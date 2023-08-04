914d0ab371062ac3ab71727f674aef79e7daa2d8
Rohan <cipher@rohannk.com>
2023-08-04 11:41:34 +0530

    Roman validation

--- 2nd Year/4th Sem/Python-Lab/p4-b.py
+++ 2nd Year/4th Sem/Python-Lab/p4-b.py
@@ -1,3 +1,8 @@
+import re
+
+def validate_roman(string):
+    return bool(re.search(pattern r"^M{0,3}(CM|CD|D>C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3}$",string))
+
 def roman_to_int(s):
     rom_dict = {'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000}
     total = 0
@@ -12,4 +17,8 @@ def roman_to_int(s):
         prev_val = cur_val
     return total
 
-print(roman_to_int("IV"))
+rom = input("Enter roman number : ")
+if validate_roman(rom):
+    print(roman_to_int(rom))
+else:
+    print("Invalid roman number")
