cb876dd71e1d3b5944b48ca8685b1b2ee52d0ced
Rohan <cipher@rohannk.com>
2023-08-25 12:22:46 +0530

    prog9 -half

---
+++ 2nd Year/4th Sem/Python-Lab/p9-a.py
@@ -0,0 +1,30 @@
+import os
+import bs4
+import requests
+
+url = 'https://xkcd.com'
+os.makedirs('xkcd', exist_ok=True)
+
+while not url.endswith('#'):
+	print("Downloading the page .....")
+	res = requests.get(url)
+	res.raise_for_status()
+
+try:
+	soup = bs4.BeautifulSoup(res.text, 'html.parser')
+	comic_element = soup.select('#comic img')
+	if comic_element == []:
+		print("No comic image found !")
+	else:
+		comic_image_url = comic_element[0].get('src')
+		print("Downloading the image %s..." % (comic_image_url))
+		res = requests.get('http:' + comic_image_url)
+		res.raise_for_status()
+		file = open(os.path.join('xkcd',os.path.basename(comic_image_url)))
+		for chunk in res.iter_content(10):
+			file.write(chunk)
+			file.close()
+
+prev_link = soup.select('a[rel="prev')[0]
+url = 'https://xkcd.com'+prev_link.get('href')
+print("Done")
