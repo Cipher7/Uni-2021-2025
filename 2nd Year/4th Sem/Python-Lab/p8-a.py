8f93a6ec840bd03206627d5fc9688e5182668db6
Rohan <cipher@rohannk.com>
2023-08-25 11:08:59 +0530

    Program 8

---
+++ 2nd Year/4th Sem/Python-Lab/p8-a.py
@@ -0,0 +1,51 @@
+class Palindrome:
+    def __init__(self,data):
+        self.data = data
+        self.isPalindrome = False
+
+    def data_palindrome(self):
+        if self.data == self.data[::-1]:
+            self.isPalindrome = True
+        else:
+            self.isPalindrome = False
+
+        return self.isPalindrome
+
+class StrPalindrome(Palindrome):
+    pass
+
+class IntPalindrome(Palindrome):
+    def __init__(self, num):
+        self.num = num
+        self.isPalindrome = False
+
+    def data_palindrome(self):
+        temp = self.num
+        rev = 0
+        while temp != 0:
+            d = temp % 10
+            rev = (rev*10) + d
+            temp = temp // 10
+
+        if rev == self.num:
+            self.isPalindrome = True
+        else:
+            self.isPalindrome = False
+
+        return self.isPalindrome
+
+string = input("Enter the input string : ")
+str_obj = StrPalindrome(string)
+
+if str_obj.data_palindrome():
+    print("String entered is a palindrome")
+else:
+    print("String entered is not a palindrome")
+
+number = int(input("Enter a number : "))
+num_obj = IntPalindrome(number)
+
+if num_obj.data_palindrome():
+    print("Number entered is a palindrome")
+else:
+    print("Number entered is not a palindrome")
