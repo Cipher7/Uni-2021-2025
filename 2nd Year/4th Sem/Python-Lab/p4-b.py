3c9bad2ff67aa1e9a68c232733eecc3e2f366bbb
Rohan N K <70212373+Cipher7@users.noreply.github.com>
2023-09-21 00:16:22 +0530

    Update p4-b.py

--- 2nd Year/4th Sem/Python-Lab/p4-b.py
+++ 2nd Year/4th Sem/Python-Lab/p4-b.py
@@ -1,7 +1,7 @@
 import re
 
 def validate_roman(string):
-    return bool(re.search(pattern r"^M{0,3}(CM|CD|D>C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3}$",string))
+    return bool(re.search(pattern r"^M{0,3}(CM|CD|D|C{0,3})(XC|XL|L|X{0,3})(IX|IV|V|I{0,3})$",string))
 
 def roman_to_int(s):
     rom_dict = {'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000}
