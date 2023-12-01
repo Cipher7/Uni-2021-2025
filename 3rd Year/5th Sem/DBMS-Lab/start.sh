0ad88fafb81e840b4a30c38606cb3e151cca9df9
rohan <rohan@college.com>
2023-12-01 15:23:09 +0530

    update to script

--- 3rd Year/5th Sem/DBMS-Lab/start.sh
+++ 3rd Year/5th Sem/DBMS-Lab/start.sh
@@ -9,7 +9,8 @@
 # create table student(usn int primary key, name varchar(40), dob date, marks int, gender char);
 # show tables;
 # insert into student values(01, "mark", "2000-11-7", 69, 'M');
-#
+# desc student;
+# select * from student;
 # -------------------------------------------------------------------------------------------------
 
 # Start lampp server
