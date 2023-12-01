ab36869de1ca86cf6535b4f4fe756551ce5d1878
rohan <rohan@college.com>
2023-12-01 15:17:55 +0530

    DBMS Lab

---
+++ 3rd Year/5th Sem/DBMS-Lab/start.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# --------------------- NOTES-------------------------------------------------------------------
+# We created a database called 'college4' with the table called 'student'
+# Commands used --> 
+# show databases;
+# create database college4;
+# show databases;
+# create table student(usn int primary key, name varchar(40), dob date, marks int, gender char);
+# show tables;
+# insert into student values(01, "mark", "2000-11-7", 69, 'M');
+#
+# -------------------------------------------------------------------------------------------------
+
+# Start lampp server
+sudo /opt/lampp/manager-linux-x64.run &
+
+# Start the mysql server
+sudo /opt/lampp/bin/mysql -u root -D college4
