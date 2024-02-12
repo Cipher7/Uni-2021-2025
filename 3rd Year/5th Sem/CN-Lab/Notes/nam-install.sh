3555820cac1daba39518a47f42aaa7ca5f08ecd5
rohan <rohan@cnlab.com>
2024-02-12 15:21:17 +0530

    nam install script

---
+++ 3rd Year/5th Sem/CN-Lab/Notes/nam-install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+sudo apt remove nam
+wget https://github.com/Cipher7/Uni-2021-2025/raw/main/3rd%20Year/5th%20Sem/CN-Lab/nam_1.14_amd64.deb
+sudo dpkg -i nam_1.14_amd64.deb
+echo "NAM 1.14 Ready!!"
