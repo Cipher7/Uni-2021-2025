7d6701bb003a716b0c7f23101d0f1cf053f4340d
Rohan <roka21cs@cmrit.ac.in>
2024-06-12 11:40:25 +0530

    3d transformations

--- 3rd Year/6th Sem/CG-Lab/program4.cpp
+++ 3rd Year/6th Sem/CG-Lab/program4.cpp
@@ -16,7 +16,16 @@ void divide_triangle(float a[3], float b[3], float c[3], int k)
 {
 	if(k>0)
 	{
-
+		float m1[3], m2[3], m3[3];
+		for(int i=0;i<3;i++)
+		{
+			m1[i] = (a[i] + b[i])/2;
+			m2[i] = (a[i] + c[i])/2;
+			m3[i] = (b[i] + c[i])/2;
+		}
+		divide_triangle(a,m1,m2,k-1);
+		divide_triangle(b,m1,m3,k-1);
+		divide_triangle(c,m2,m3,k-1);
 	}
 	else
 		triangle(a,b,c);
@@ -24,21 +33,26 @@ void divide_triangle(float a[3], float b[3], float c[3], int k)
 
 void tetrahedron()
 {
+	glColor3f(0,0,0);
+	divide_triangle(v[1], v[2], v[3], k);
+
 	glColor3f(1,0,0);
 	divide_triangle(v[0], v[1], v[2], k);
-
-	glColor3f(0,1,0);
-	divide_triangle(v[1], v[2], v[3], k);
 
 	glColor3f(0,0,1);
 	divide_triangle(v[0], v[1], v[3], k);
 
+	glColor3f(1,1,0);
+	divide_triangle(v[0], v[2], v[3], k);
 }
 
 void display()
 {
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glPushMatrix();
+//	glRotatef(45,1,0,0);
 	tetrahedron();
+	glPopMatrix();
 	glFlush();
 }
 
@@ -46,12 +60,14 @@ void myinit()
 {
 	glClearColor(1,1,1,1);
 	glMatrixMode(GL_PROJECTION);
-	glOrtho(-2,2,-2,2,-2,2);
+	glOrtho(-2,2,-2,2,-3,3);
 	glMatrixMode(GL_MODELVIEW);
 }
 
 int main(int argc, char** argv)
 {
+	cout << "Enter the number of subdivisions : ";
+	cin >> k;
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_RGB | GLUT_SINGLE | GLUT_DEPTH);
 	
