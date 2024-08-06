bb1859f408c2b221432279f3ef15cc249d6a9877
Rohan N K <70212373+Cipher7@users.noreply.github.com>
2024-08-06 16:21:28 +0530

    Update prog9.py

--- 3rd Year/6th Sem/CG-Lab/prog9.py
+++ 3rd Year/6th Sem/CG-Lab/prog9.py
@@ -7,3 +7,11 @@ img=cv.imread('messi.jpeg')
 img_gray=cv.cvtColor(img,cv.COLOR_BGR2GRAY)
 img_edges=cv.Canny(img,threshold1=100,threshold2=200)
 plt.imshow(img_edges)
+
+img = cv.imgread('chess.png')
+img_gray=cv.cvtColor(img,cv.COLOR_BGR2GRAY)
+img_f = np.float32(img_gray)
+img_corner = cv.cornerHarris(img_f, 2,5,0.07)
+img_corner = cv.dialate(img_corner, None)
+img[img_corner > 0.01*img_corner.max()] = [0,0,255]
+plt.imshow(img)
